ddba4155b5d5cba4c46088b1fc6345863d10b849
kuroedov_maxim <Ni7pfpNh>
2018-08-22 23:44:33 +0300

    pixel-perfect main page

--- js/check.js
+++ js/check.js
@@ -1,1 +1,1 @@
-transform: perspective(1200px) rotateX(0deg) rotateY(0deg) translateZ(0px) translateX() translateY();
+// transform: perspective(1200px) rotateX(0deg) rotateY(0deg) translateZ(0px) translateX() translateY();
